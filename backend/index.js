aa5f89f12740b9a5bfac3c344da8937f2e22cb94
SaddamTechie <saddamtechie@gmail.com>
2025-04-06 13:23:59 +0300

    added vercel.json

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,10 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error(err));
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use('/api/clearance', clearanceRoutes);
 
 // Socket.IO connection
